d7d75ee36c8241d0a4b9f2fa007c7c930fecfd01
Nikolausz László Simon <79383901+nikoteszt@users.noreply.github.com>
2021-08-13 22:30:38 +0200

    Update test_listing_data.py

--- testproject/test_listing_data.py
+++ testproject/test_listing_data.py
@@ -2,6 +2,7 @@ import pytest
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 chrome_options = Options()
 chrome_options.headless = True
 
