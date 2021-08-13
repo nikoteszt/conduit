21e9baf524c378dcda8bbba979e2bb21811cf65e
Nikolausz László Simon <79383901+nikoteszt@users.noreply.github.com>
2021-08-13 14:14:00 +0200

    Add test_logout.py

---
+++ testproject/test_logout.py
@@ -0,0 +1,31 @@
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+chrome_options = Options()
+chrome_options.headless = True
+
+
+class TestLogout(object):
+    def setup_method(self):
+        self.driver = webdriver.Chrome(options=chrome_options)
+        self.driver.get("http://localhost:1667/")
+        time.sleep(3)
+        self.driver.find_element_by_link_text("Sign in").click()
+        self.driver.find_element_by_xpath('//input[@placeholder="Email"]').send_keys("ntest2@ceg.hu")
+        self.driver.find_element_by_xpath('//input[@placeholder="Password"]').send_keys("ntest222A")
+        self.driver.find_element_by_class_name("btn-primary").click()
+        time.sleep(3)
+
+    def teardown_method(self):
+        self.driver.close()
+        self.driver.quit()
+
+    def test_logout(self):
+        len_ntest2_link = len(self.driver.find_elements_by_link_text('ntest2'))
+        print(len_ntest2_link)
+        assert len_ntest2_link > 0
+        self.driver.find_element(By.LINK_TEXT, "Log out").click()
+        time.sleep(3)
+        assert len(self.driver.find_elements_by_link_text('ntest2')) == (len_ntest2_link - 1)
