0e67efe85d0d4373be8b33862232b40e31d0c154
Nikolausz László Simon <79383901+nikoteszt@users.noreply.github.com>
2021-08-14 03:09:53 +0200

    Fixed registration.py

--- testproject/preconfig/registration.py
+++ testproject/preconfig/registration.py
@@ -22,7 +22,7 @@ class TestRegistration(object):
         self.driver.find_element_by_xpath('//input[@type="password"]').send_keys("ntest222A")
         self.driver.find_element(By.XPATH, "//button[contains(.,\'Sign up\')]").click()
         time.sleep(2)
-        reg = self.driver.find_element_by_xpath('//div[text()="Your registration was successful!]')
+        reg = self.driver.find_element_by_xpath('//div[text()="Your registration was successful!"]')
         assert len(reg) > 0
         self.driver.find_element(By.XPATH, "//button[contains(.,\'OK\')]").click()
         elements = self.driver.find_element_by_link_text("ntest2")
