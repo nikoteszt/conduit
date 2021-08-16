2e47257563cc2b01079d45db0e3e4011ecb9c1c0
Nikolausz László Simon <79383901+nikoteszt@users.noreply.github.com>
2021-08-16 16:22:00 +0200

    test_multiple_pages.py update comments

--- testproject/test_multiple_pages.py
+++ testproject/test_multiple_pages.py
@@ -29,15 +29,22 @@ class TestMultiplePages(object):
     def test_tc06_multiple_pages(self):
         self.driver.find_element_by_link_text("Your Feed").click()
         time.sleep(1)
+
+        # Összegyüjtjük az oldalak linkjét
         page_link = self.driver.find_elements_by_class_name("page-link")
-        fpa = []
+
+        fpa = []  # ebben tároljuk az oldalakon levő első bejegyzések címét
         for i in range(len(page_link)):
             feed_pages_article = self.driver.find_elements_by_class_name("article-preview")
             assert len(feed_pages_article) > 0
             print(f"Az {i+1}.oldalon levő bejegyzések száma: ", len(feed_pages_article))
             fpa.append(feed_pages_article[0].find_element_by_xpath('//a/h1').text)
+            
+            # A 2. oldaltól kezdve összehasonlítjuk az oldal első bejegyzését, az elöző oldal első bejegyzésével.
             if i > 0:
                 assert fpa[i] != fpa[i-1]
+
+            # ha még nem vagyunk az utolsó oldalon, lapozunk
             if i < len(page_link)-1:
                 self.driver.find_element_by_xpath(f'//a[@class="page-link"][text()="{i+2}"]').click()
             time.sleep(3)
